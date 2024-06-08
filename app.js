4af61ea23a781a41118ac3e9da5591e129e7273d
anndrelipe <ppeixoto.andre@gmail.com>
2024-06-07 21:36:32 -0300

    Implement GET and POST methods for conta

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 import express from "express"
 const app = express()
 const port = 3000
+app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('API funcionando, tente /conta ou /transacao!')
 })
+
+app.post('/conta', (req, res) => {
+
+  const { conta_id, valor } = req.body
+
+    res.status(201).json({
+      conta_id: 1234,
+      saldo: 189.70
+    })
+  })
+
+
+app.get('/conta/:id', (req, res) => {
+  const id = req.params.id 
+  if (id != 1234) {
+    res.status(404).send(`Erro, conta de id ${id} não encontrada.`)
+  } else {
+    res.status(200).json(
+      {
+        conta_id: 1234, 
+        saldo: 200
+      }
+    )
+  }
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
