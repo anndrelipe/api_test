3e40bc0eb90aec1494829a710ca912ba26e4ad51
anndrelipe <ppeixoto.andre@gmail.com>
2024-06-21 14:34:55 -0300

    Integra o banco de dados ao get/conta

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
+import 'dotenv/config'
 import express from "express"
+import mysql from 'mysql'
+
 const app = express()
-const port = 3000
+const port = 8000
+
 app.use(express.json())
+
+var db = mysql.createConnection({
+  database : process.env.DB_NAME,
+  port     : process.env.DB_BASE_PORT,
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_ROOT_PASSWORD
+});
+
+
+db.connect(err => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados: ', err);
+    return;
+  }
+  console.log('Conectado ao banco de dados MySQL');
+});
+
 
 app.get('/', (req, res) => {
   res.send('API funcionando, tente /conta ou /transacao!')
@@ -24,7 +46,7 @@ app.post('/transacao', (req, res) => {
 app.post('/conta', (req, res) => {
 
   const { conta_id, valor } = req.body
-
+  
     res.status(201).json({
       conta_id: 1234,
       saldo: 189.70
@@ -33,19 +55,20 @@ app.post('/conta', (req, res) => {
 
 
 app.get('/conta/:id', (req, res) => {
-  const id = req.params.id 
-  if (id != 1234) {
-    res.status(404).send(`Erro, conta de id ${id} não encontrada.`)
-  } else {
-    res.status(200).json(
-      {
-        conta_id: 1234, 
-        saldo: 200
-      }
-    )
-  }
+  const id = req.params.id;
+  const query = `SELECT * FROM conta WHERE conta_id = ${id}`;
+
+  db.query(query, function (err, result) {
+    if (err || result.length === 0) {
+      res.status(404).json({mensagem : "Algo deu errado :( Verifique se o id existe!"})
+      return
+    }
+    res.status(200).json(result[0])
   })
 
-app.listen(port, () => {
+})
+
+
+  app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
